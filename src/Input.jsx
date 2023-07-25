1d26c45e0ec27f8ca38a526be40ed017af9a5e5f
Saidumarov <saidumarovjafarxon@gmail06.com>
2023-07-25 10:34:50 +0500

    git push

--- src/Input.jsx
+++ src/Input.jsx
@@ -60,6 +60,8 @@ const Input = () => {
     document.querySelector("modal , h2").style.color = "";
     document.querySelector(".img").style.zIndex = "-1";
     document.querySelector(".img1").style.zIndex = "1";
+    document.getElementById('delete').style.backgroundColor = "";
+    document.getElementById('edit').style.backgroundColor = "";
   };
 
   const handleButtonClick1 = () => {
@@ -76,6 +78,9 @@ const Input = () => {
     document.querySelector(".img1").style.zIndex = "-1";
     document.querySelector(".img").style.zIndex = "1";
     document.querySelector(".dum").style.backgroundColor = "rgb(204, 210, 215)";
+    document.getElementById('delete').style.backgroundColor = "white";
+    document.getElementById('edit').style.backgroundColor = "white";
+
   };
 
   const filter = Names.filter(
@@ -189,14 +194,8 @@ const Input = () => {
                   <span className="s">{age}</span>
                 </p>
               </div>
-              <button onClick={() => handleEdit({ firstName, lastName, age })}>
-                Edit
-              </button>
-              <button
-                onClick={() => handleDelete({ firstName, lastName, age })}
-              >
-                Delete
-              </button>
+              <i class="fa-solid fa-trash"  id="delete"  onClick={() => handleDelete({ firstName, lastName, age })}></i>
+              <i class="fa-solid fa-pen-to-square" id="edit"  onClick={() => handleEdit({ firstName, lastName, age })}></i>
             </div>
           ))}
         </div>
